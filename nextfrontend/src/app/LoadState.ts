8c0b78e9046e54baedc333838c5e68519df0e910
jwaldor <98110682+jwaldor@users.noreply.github.com>
2024-11-05 11:33:23 -0500

    set up local storage

--- nextfrontend/src/app/LoadState.ts
+++ nextfrontend/src/app/LoadState.ts
@@ -1,17 +1,17 @@
-import { useEffect } from "react";
-import useTaskStore from "./tasks";
+// import { useEffect } from "react";
+// import useTaskStore from "./tasks";
 
-export function useLoadState() {
-  return useEffect(() => {
-    console.log("LoadState");
-    const store = localStorage.getItem("store");
-    if (store) {
-      useTaskStore.setState(JSON.parse(store));
-    }
-  }, []);
-}
+// export function useLoadState() {
+//   return useEffect(() => {
+//     console.log("LoadState");
+//     const store = localStorage.getItem("store");
+//     if (store) {
+//       useTaskStore.setState(JSON.parse(store));
+//     }
+//   }, []);
+// }
 
-export function useSaveState() {
-  const store = useTaskStore();
-  localStorage.setItem("store", JSON.stringify(store));
-}
+// export function useSaveState() {
+//   const store = useTaskStore();
+//   localStorage.setItem("store", JSON.stringify(store));
+// }
