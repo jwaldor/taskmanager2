ec79f790b809e38997025d885a62a03896e3875c
jwaldor <98110682+jwaldor@users.noreply.github.com>
2024-11-06 12:19:46 -0500

    display tasks for an epic underneath the epic

--- nextfrontend/src/app/epics/page.tsx
+++ nextfrontend/src/app/epics/page.tsx
@@ -11,14 +11,20 @@ import {
 import { Button } from "@/components/ui/button"
 import { EpicRow } from "../components/Epic"
 import useTaskStore from '../tasks' // Adjust the import path based on your project structure
+import { TaskRow } from "../components/Task"
+import { Fragment } from "react"
 
 export default function Epics() {
-  const { epics, createEpic } = useTaskStore()
+  const { epics, createEpic, tasks } = useTaskStore()
+  const { editingCell, editValue, setEditValue, cancelEditing, saveEdit, startEditing, themes, currentTheme, deleteTask } = useTaskStore()
 
+  const getTasksForEpic = (epicId: string) => {
+    return tasks.filter(task => task.epic?.id === epicId)
+  }
   return (
     <div className="w-full h-full overflow-auto">
       <Table>
-        <TableCaption>A list of your editable tasks.</TableCaption>
+        <TableCaption>A list of your editable epics.</TableCaption>
         <TableHeader>
           <TableRow>
             <TableHead className="w-[200px]">Name</TableHead>
@@ -28,7 +34,12 @@ export default function Epics() {
         <TableBody>
           {epics.map((epic, index) => {
             return (
-              <EpicRow key={index} task={epic} rowIndex={index} />
+              <Fragment key={epic.id}>
+                <EpicRow key={index} task={epic} rowIndex={index} />
+                {getTasksForEpic(epic.id).map((task, taskIndex) => {
+                  return <TaskRow key={epic.id + taskIndex} task={task} rowIndex={taskIndex} editingCell={editingCell} editValue={editValue} setEditValue={setEditValue} cancelEditing={cancelEditing} saveEdit={saveEdit} startEditing={startEditing} themes={themes} currentTheme={currentTheme} deleteTask={deleteTask} />
+                })}
+              </Fragment>
             )
           })}
         </TableBody>
